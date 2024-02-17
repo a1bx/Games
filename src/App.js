8b5c47d9fa59053a8c12667e4f79882050acab9d
Jeremiah Nyok <jeremih@students.uonbi.ac.ke>
2024-02-17 17:00:43 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Home from './components/home';
+import Nav from './components/nav';
 
 function App() {
   return (
     <div className="App">
+      <Nav/>
       <Home />
     </div>
   );
