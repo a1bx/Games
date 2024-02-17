3d5b1b4857b5f6879bd35ecc7bf4a50af129ab38
Jeremiah Nyok <jeremih@students.uonbi.ac.ke>
2024-02-17 17:03:34 +0300

    update

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,78 +1,19 @@
-import React, { useState } from 'react';
-import Background from '../assets/background.jpg';
+import React from 'react';
+import Abstract from '../assets/abstract.mp4';
 
 const Home = () => {
-  const [menuOpen, setMenuOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setMenuOpen(!menuOpen);
-  };
-
   return (
-    <div>
-      {/* Navigation */}
-      <nav className="bg-black p-4 relative">
-        <div className="container mx-auto flex items-center justify-between">
-          <div className="text-white">
-            <h1 className="text-xl text-white font-bold">GHINIUD</h1>
-          </div>
-          <div className="hidden md:flex space-x-4 ">
-            <a href="#" className="text-red-400">
-              Home
-            </a>
-            <a href="#" className="text-white">
-              About
-            </a>
-            <a href="#" className="text-white">
-              Contact
-            </a>
-          </div>
-          <div className="md:hidden">
-            <button
-              className="text-white focus:outline-none"
-              onClick={toggleMenu}
-            >
-              <svg
-                className="h-6 w-6"
-                fill="none"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path d="M4 6h16M4 12h16m-7 6h7"></path>
-              </svg>
-            </button>
-          </div>
-        </div>
-      </nav>
-      
-
-      {/* Mobile Navigation Menu */}
-      {menuOpen && (
-        <div className="md:hidden bg-black p-4">
-          <a href="#" className="block text-red-600 py-2">
-            Home
-          </a>
-          <a href="#" className="block text-red-600 py-2">
-            About
-          </a>
-          <a href="#" className="block text-red-600 py-2">
-            Contact
-          </a>
-        </div>
-      )}
-      <div
-        className="h-screen flex items-center justify-center text-white"
-        style={{
-          background: `url(${Background}) center/cover no-repeat`,
-        }}
-      >
-        <h1 className="text-4xl font-bold text-red-600 relative z-10">Gaming Wave</h1>
-      </div>
+    <div className='home'>
+      <video src={Abstract} autoplay loop muted/>
     </div>
+    // <div
+    //   className="h-screen flex items-center justify-center text-white"
+    //   style={{
+    //     background: `url(${Background}) center/cover no-repeat`,
+    //   }}
+    // >
+    //   <h1 className="text-4xl font-bold text-red-600 relative z-10">Gaming Wave</h1>
+    // </div>
   );
 };
-
 export default Home;
