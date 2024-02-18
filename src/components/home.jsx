d5f47650e31e3c71e5a50ef24c77bd115912d988
Jeremiah Nyok <jeremih@students.uonbi.ac.ke>
2024-02-18 22:45:47 +0300

    update

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import Abstract from '../assets/abstract.mp4';
+import Background from '../assets/background.jpg';
 
 const Home = () => {
   return (
-    <div className='home'>
-      <video src={Abstract} autoplay loop muted/>
-    </div>
-    // <div
-    //   className="h-screen flex items-center justify-center text-white"
-    //   style={{
-    //     background: `url(${Background}) center/cover no-repeat`,
-    //   }}
-    // >
-    //   <h1 className="text-4xl font-bold text-red-600 relative z-10">Gaming Wave</h1>
+    // <div className='home'>
+    //   <video src={Abstract} autoplay loop muted/>
     // </div>
+    <div
+      className="h-screen flex items-center justify-center text-white"
+      style={{
+        background: `url(${Background}) center/cover no-repeat`,
+      }}
+    >
+      <h1 className="text-4xl font-bold text-red-600 absolute z-10">Gaming Wave</h1>
+    </div>
   );
 };
 export default Home;
